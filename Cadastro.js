a5ae92b13845fa8cabe305538c43b703470d6c65
Rodolfo Berwanger Liberado <83656054+RodBerw@users.noreply.github.com>
2023-04-14 20:07:21 -0300

    React exemplo de página de cadastro

---
+++ Cadastro.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+
+export default function Cadastro(){
+    const [nome, setNome] = useState('');
+    const [telefone, setTelefone] = useState('');
+
+    function handleNomeChange(event) {
+        setNome(event.target.value);
+    }
+    function handleTelefoneChange(event) {
+        setTelefone(event.target.value);
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log(`Nome: ${nome}, Telefone: ${telefone}`);
+    }
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+            <label>
+                Nome:
+                <input type="text" value={nome} onChange={handleNomeChange}/>
+            </label>
+            <br/>
+            <label>
+                Telefone:
+                <input type="text" value={telefone} onChange={handleTelefoneChange}/>
+            </label>
+            <br/>
+            <button type="submit">Enviar</button>
+            </form>
+        </div>
+    )
+}
